86fa14ea6462344e088b8e2688e68691f82f964e
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:43:39 +0530

    Update program9_1.c

--- Assignments/Assignment_09/program9_1.c
+++ Assignments/Assignment_09/program9_1.c
@@ -6,7 +6,7 @@
 //  Description :   To get pattern in form * # 
 //  Input :         int
 //  Output :        string
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Tejas Sunil Ahirrao
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
