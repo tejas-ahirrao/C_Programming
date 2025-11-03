8c62171cc5c47b8885640ce86fb439aeb0cf0e62
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:42:25 +0530

    Update program7_3.c

--- Assignments/Assignment_07/program7_3.c
+++ Assignments/Assignment_07/program7_3.c
@@ -6,7 +6,7 @@
 //  Description :   To print numberline of the input number
 //  Input :         int
 //  Output :        void
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Tejas Sunil Ahirrao
 //  Date :          26/10/2025
 //
 //////////////////////////////////////////////////////////////
