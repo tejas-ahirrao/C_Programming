6c3dcbe225c4a9299b78fe086a7ec7732d0a5aaf
pansareprajwal <153760375+pansareprajwal@users.noreply.github.com>
2025-11-03 22:22:35 +0530

    Update

--- program1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{printf("jai ganesh...\n");
-
-
-
-    return 0;
-}
--- program4.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-
-    int i=10, j=11, ans=0;
-    ans=i+j;
-    return 0;
-}
