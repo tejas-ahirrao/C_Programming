0a51eccd99dd150bd628b0de69427b904e9d9e78
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:41:03 +0530

    Update program5_5.c

--- Assignments/Assignment_05/program5_5.c
+++ Assignments/Assignment_05/program5_5.c
@@ -6,7 +6,7 @@
 //  Description :   To find largest among three numbers
 //  Input :         int, int, int
 //  Output :        int
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Tejas Sunil Ahirrao
 //  Date :          26/10/2025
 //
 //////////////////////////////////////////////////////////////
