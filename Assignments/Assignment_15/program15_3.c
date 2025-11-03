8ebb64b7e5ad0f6f0b21ac05ce2ea1f76758ee6b
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:49:37 +0530

    Update program15_3.c

--- Assignments/Assignment_15/program15_3.c
+++ Assignments/Assignment_15/program15_3.c
@@ -6,7 +6,7 @@
 //  Description :   To check frequency of digits between 3 & 7 in number
 //  Input :         int
 //  Output :        int
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Tejas Sunil Ahirrao
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
