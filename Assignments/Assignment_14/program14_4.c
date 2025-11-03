a68ea141369fd09da6a6ba27b73b74ff2f692ec6
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:48:44 +0530

    Update program14_4.c

--- Assignments/Assignment_14/program14_4.c
+++ Assignments/Assignment_14/program14_4.c
@@ -6,7 +6,7 @@
 //  Description :   To check frequency of 4 in number
 //  Input :         int
 //  Output :        void
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Tejas Sunil Ahirrao
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
