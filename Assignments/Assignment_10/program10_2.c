d54b2b0e769ceb19ddba2e8d8060fb034679b7bd
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:44:33 +0530

    Update program10_2.c

--- Assignments/Assignment_10/program10_2.c
+++ Assignments/Assignment_10/program10_2.c
@@ -6,7 +6,7 @@
 //  Description :   To get area of the Rectangle
 //  Input :         float, float
 //  Output :        float
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Tejas Sunil Ahirrao
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
