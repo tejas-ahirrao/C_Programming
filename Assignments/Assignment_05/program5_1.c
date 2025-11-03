1d12ca000f57975c5c3fb17a992dd73b6c821de7
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:40:18 +0530

    Update program5_1.c

--- Assignments/Assignment_05/program5_1.c
+++ Assignments/Assignment_05/program5_1.c
@@ -6,7 +6,7 @@
 //  Description :   To check Even or Odd
 //  Input :         int
 //  Output :        void
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Tejas Sunil Ahirrao
 //  Date :          26/10/2025
 //
 //////////////////////////////////////////////////////////////
