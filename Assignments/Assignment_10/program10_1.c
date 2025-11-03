875ac92c860ac3efff409127783550d76277f370
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:44:25 +0530

    Update program10_1.c

--- Assignments/Assignment_10/program10_1.c
+++ Assignments/Assignment_10/program10_1.c
@@ -6,7 +6,7 @@
 //  Description :   To get area of the circle
 //  Input :         float
 //  Output :        float
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Tejas Sunil Ahirrao
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
