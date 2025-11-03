49fb2854ac25b5b138c24d390baf940e30c74f8b
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:44:17 +0530

    Update program9_5.c

--- Assignments/Assignment_09/program9_5.c
+++ Assignments/Assignment_09/program9_5.c
@@ -6,7 +6,7 @@
 //  Description :   To get facotrial differcne of input
 //  Input :         int
 //  Output :        int
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Tejas Sunil Ahirrao
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
